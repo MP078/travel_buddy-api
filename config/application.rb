fd94ed706c49bf78ef03437ec19e99d9908ef355
Seamoon Pandey <hackedasocial@gmail.com>
2025-05-14 14:23:57 +0545

    add cookie support and update DeviseTokenAuth configuration

--- config/application.rb
+++ config/application.rb
@@ -44,9 +44,17 @@ module TravelbuddyApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    # Generators
     config.generators do |generate|
       generate.orm :active_record, primary_key_type: :uuid
       generate.test_framework :rspec
     end
+
+
+    # Middleware for cookies
+    config.api_only = true
+    config.session_store :cookie_store, key: "_travelbuddy_api_session", httponly: true, same_site: :strict, secure: true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
