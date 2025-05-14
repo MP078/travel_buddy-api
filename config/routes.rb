4726f37438884e72f579b45afbd06e50fd12ea0b
Seamoon Pandey <hackedasocial@gmail.com>
2025-05-14 17:01:26 +0545

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   # User routes
   resources :users, param: :username, only: %i[index show update]
 
+  # User posts routes
+  resources :posts
+
   # Rails health check
   get "up" => "rails/health#show", as: :rails_health_check
 end
