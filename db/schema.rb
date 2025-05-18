0dd7963b75ed6fb3d09be77d68312984d20df36d
Seamoon Pandey <hackedasocial@gmail.com>
2025-05-18 06:57:51 +0545

    refactor: remove chat messages functionality and related files

--- db/schema.rb
+++ db/schema.rb
@@ -45,17 +45,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_163850) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "chat_messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "sender_id", null: false
-    t.uuid "receiver_id", null: false
-    t.text "content"
-    t.boolean "read"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["receiver_id"], name: "index_chat_messages_on_receiver_id"
-    t.index ["sender_id"], name: "index_chat_messages_on_sender_id"
-  end
-
   create_table "comments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "body"
     t.uuid "user_id", null: false
@@ -217,8 +206,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_163850) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "chat_messages", "users", column: "receiver_id"
-  add_foreign_key "chat_messages", "users", column: "sender_id"
   add_foreign_key "comments", "comments", column: "parent_id"
   add_foreign_key "comments", "users"
   add_foreign_key "friendships", "users", column: "receiver_id"
