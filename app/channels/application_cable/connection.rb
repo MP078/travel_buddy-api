1e5070628a26151fbd803fbd9b70c97904c5d51b
Seamoon Pandey <hackedasocial@gmail.com>
2025-05-25 07:46:44 +0545

    try to auth

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,12 +10,27 @@ module ApplicationCable
 
     private
       def find_verified_user
-        # Devise/Warden session-based authentication
-        if env["warden"].user
-          env["warden"].user
-        else
-          reject_unauthorized_connection
+        # Try session-based authentication first
+        session = cookies.encrypted["_travelbuddy_api_session"]
+        user_id = session && session["user_id"]
+        return User.find_by(id: user_id) if user_id
+
+        # Try token-based authentication (from params or cookie)
+        token = request.params["token"] || cookies["auth_cookie"]
+        Rails.logger.debug "ActionCable token: #{token.inspect}"
+
+        if token
+          begin
+            data = JSON.parse(CGI.unescape(token)) rescue nil
+            Rails.logger.debug "Parsed token data: #{data.inspect}"
+            user = User.find_by(email: data["uid"]) if data && data["uid"]
+            return user if user
+          rescue => e
+            Rails.logger.error "Token parse error: #{e.message}"
+          end
         end
+
+        reject_unauthorized_connection
       end
   end
 end
